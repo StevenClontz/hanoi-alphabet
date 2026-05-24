831f2d06a51e5bf44b1653303a679d7979afecf5
Steven Clontz <steven.clontz@gmail.com>
2026-05-24 14:53:16 +0000

    tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+	// import {useDraggable} from '@dnd-kit-svelte/svelte';
+    // import type { Attachment } from 'svelte/attachments';
+
     interface DiscI {
         letter: string;
         size: number;
+        // ref: Attachment;
     }
     const discs:DiscI[] = [
-        {letter: "A", size: 1},
-        {letter: "B", size: 2},
-        {letter: "C", size: 3},
+        {letter: "A", size: 1},//, ref: useDraggable({id: "A"}).ref},
+        {letter: "B", size: 2},//, ref: useDraggable({id: "B"}).ref},
+        {letter: "C", size: 3},//, ref: useDraggable({id: "C"}).ref},
     ]
     const discWidth = (disc:DiscI) => {
         return 10*disc.size + (95 - 10*Math.max(...discs.map(d=>d.size)))
@@ -15,15 +19,21 @@
         discs: DiscI[];
         color: string;
     }
-    const towers:TowerI[] = [
-        {discs: Array.from(discs), color: "blue"},
-        {discs: [], color: "red"},
-        {discs: [], color: "green"},
-    ]
+    interface TowersI {
+        left: TowerI
+        middle: TowerI
+        right: TowerI
+    }
+    const towers:TowersI = {
+        left: {discs: Array.from(discs), color: "blue"},
+        middle: {discs: [], color: "red"},
+        right: {discs: [], color: "green"},
+    }
 </script>
 
 {#snippet Disc(disc:DiscI)}
-<div class="text-center rounded-md my-2 mx-auto border bg-gray-100 cursor-pointer select-none p-1" style="width: {discWidth(disc)}%">
+<div class="text-center rounded-md my-2 mx-auto border bg-gray-100 cursor-pointer select-none p-1" 
+    style="width: {discWidth(disc)}%">
     <input type="text" bind:value={disc.letter} maxlength="1" class="inline-block w-6 mx-auto text-center"/>
 </div>
 {/snippet}
@@ -38,7 +48,7 @@
 </div>
 {/snippet}
 
-{#each towers as tower}
-    {@render Tower(tower)}
-{/each}
+    {@render Tower(towers.left)}
+    {@render Tower(towers.middle)}
+    {@render Tower(towers.right)}
 
