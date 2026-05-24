321fdd14305bf175198b4bd50c18bcdd9156e4d6
Steven Clontz <steven.clontz@gmail.com>
2026-05-24 14:09:47 +0000

    editable letters, width based on size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,30 @@
 <script lang="ts">
     interface DiscI {
         letter: string;
-        width: string;
+        size: number;
     }
-    let discs:DiscI[] = [
-        {letter: "A", width: "70%"},
-        {letter: "B", width: "80%"},
-        {letter: "C", width: "90%"},
+    const discs:DiscI[] = [
+        {letter: "A", size: 1},
+        {letter: "B", size: 2},
+        {letter: "C", size: 3},
     ]
+    const discWidth = (disc:DiscI) => {
+        return 10*disc.size + (95 - 10*Math.max(...discs.map(d=>d.size)))
+    }
     interface TowerI {
         discs: DiscI[];
         color: string;
     }
-    let towers:TowerI[] = [
-        {discs: discs, color: "blue"},
+    const towers:TowerI[] = [
+        {discs: Array.from(discs), color: "blue"},
         {discs: [], color: "red"},
         {discs: [], color: "green"},
     ]
 </script>
 
 {#snippet Disc(disc:DiscI)}
-<div class="text-center rounded-md my-2 mx-auto border bg-gray-100 cursor-pointer select-none p-1" style="width: {disc.width}">
-    {disc.letter}asdsfd
+<div class="text-center rounded-md my-2 mx-auto border bg-gray-100 cursor-pointer select-none p-1" style="width: {discWidth(disc)}%">
+    <input type="text" bind:value={disc.letter} maxlength="1" class="inline-block w-6 mx-auto text-center"/>
 </div>
 {/snippet}
 
